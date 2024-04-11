203b1c0f1d8eec4b613d5d33166ede1d2a60b42e
jayhshah <jayhshah@gmail.com>
2024-04-11 16:31:08 +0000

    Add version with TiledCopy vectorized load into registers and write out via TMA store

--- transpose-cute/main.cu
+++ transpose-cute/main.cu
@@ -1,6 +1,7 @@
 #include "cutlass/util/command_line.h"
 
 #include "transpose_smem.h"
+#include "transpose_tmastore_vectorized.h"
 
 int main(int argc, char const **argv) {
 
@@ -14,6 +15,7 @@ int main(int argc, char const **argv) {
   std::cout << "(M, N): " << M << ", " << N << std::endl;
 
   transpose_host_kernel_smem(M, N);
+  transpose_host_kernel_tma(M, N);
 
   return 0;
 }
